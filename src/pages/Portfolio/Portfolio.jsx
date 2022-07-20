1aa4fffe60ad09c1939be8f3e57da5b133ebadf9
Larissa da Silva de Oliveira <larissasilvaoliveira27@gmail.com>
2022-07-20 17:13:52 -0300

    incluir projeto

---
+++ src/pages/Portfolio/Portfolio.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header/Header"
+import image from "../../assets/project.svg"
+function Portfolio() {
+    return (
+        <Header
+            image={image}
+            description="ilustração mulher com site"
+        >
+            Meus Projetos
+        </Header>
+    )
+}
+export default Portfolio
