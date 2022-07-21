ab32211f0ef5217cf090612823762061013b6659
Larissa da Silva de Oliveira <larissasilvaoliveira27@gmail.com>
2022-07-21 17:39:07 -0300

    Projeto finalizado

--- src/pages/Contato/Contatos.jsx
+++ src/pages/Contato/Contatos.jsx
@@ -1,19 +1,44 @@
 import Header from "../../components/Header/Header"
 import image from "../../assets/contato.svg"
+import "./contato.css"
+import github from "../../assets/github.png"
+import linkedin from '../../assets/in.png'
+import facebook from '../../assets/facebook.png'
+import { BsArrowReturnRight } from 'react-icons/bs'
 
-function Contatos(){
-    return(
+function Contatos() {
+    return (
         <>
-         <Header
+            <Header
                 image={image}
                 description="ilustração mulher fazendo checklist"
             >
                 Contatos
             </Header>
+            <div className="container-contatos">
+                <div className="card-contato">
+                    <h3>GitHub</h3>
+                    <img src={github} alt="ícone github" />
+                    <a href="https://github.com/olaari" target="_blank">
+                        <BsArrowReturnRight color="#fff" />
+                    </a>
+                </div>
+                <div className="card-contato">
+                    <h3>Linkedin</h3>
+                    <img src={linkedin} alt="ícone linkedin" />
+                    <a href="https://www.linkedin.com/in/larissa-da-silva-de-oliveira-63b430179/" target="_blank">
+                        <BsArrowReturnRight color="#fff" />
+                    </a>
+                </div>
+                <div className="card-contato">
+                    <h3>Facebook</h3>
+                    <img src={facebook} alt="ícone facebook" />
+                    <a href="https://www.facebook.com/larissa.oliveira.37454/" target="_blank">
+                        <BsArrowReturnRight color="#fff" />
+                    </a>
+                </div>
 
-
-
-
+            </div>
         </>
     )
 }
