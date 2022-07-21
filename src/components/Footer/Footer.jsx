bbf56c7c02d425e101936892c89533d1130d3ec0
Larissa da Silva de Oliveira <larissasilvaoliveira27@gmail.com>
2022-07-21 11:47:51 -0300

    Add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './footer.css'
+
+function Footer() {
+    return (
+        <div className="texto-footer">
+            <p> Feito com carinho por Larissa Oliveira</p>
+        </div>
+    )
+}
+
+export default Footer
--- src/routes/ApplicationRoutes.jsx
+++ src/routes/ApplicationRoutes.jsx
@@ -3,6 +3,7 @@ import Sobre from '../pages/Sobre/Sobre'
 import Portfolio from '../pages/Portfolio/Portfolio'
 import Comentarios from '../pages/Comentario/Comentarios'
 import Menu from "../components/Menu/Menu"
+import Footer from '../components/Footer/Footer'
 
 function ApplicationRoutes() {
     return (
@@ -13,6 +14,7 @@ function ApplicationRoutes() {
                 <Route path="portfolio" element={<Portfolio />} />
                 <Route path="comentarios" element={<Comentarios />} />
             </Routes>
+            <Footer />
         </BrowserRouter>
     )
 }
